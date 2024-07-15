50b9e01322729d4affeca50ebca55bf1e0254f70
lapael1 <lapael6@gmail.com>
2024-07-15 18:04:54 +0900

    웹사이트 클론코딩 호버 시 확대, 명암조정, 서브메뉴 작성중

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 window.onload = function () {
   // swiper ================
   // 프로모션 스와퍼기능
-  const swPromotion = new Swiper(".sw-promotion", {
+  const swSignature = new Swiper(".sw-signature", {
     slidesPerView: 1,
     spaceBetween: 12,
     // nextEl/prevEl 안에 "".~~ .~~"" 으로 입력해주어야함
     navigation: {
-      nextEl: ".promotion .sw-next",
-      prevEl: ".promotion .sw-prev",
+      nextEl: ".signature .sw-next",
+      prevEl: ".signature .sw-prev",
     },
     loop: true,
     autoplay: {
@@ -19,29 +19,47 @@ window.onload = function () {
         slidesPerView: 2,
       },
       768: {
-        slidesPerView: 4,
+        slidesPerView: 3,
       },
     },
   });
-};
 
-window.onload = function () {
-  const bannerswiper = new Swiper(".bannerswiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
+
+const swFood = new Swiper(".sw-food", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  // nextEl/prevEl 안에 "".~~ .~~"" 으로 입력해주어야함
+  navigation: {
+    nextEl: ".food .sw-next",
+    prevEl: ".food .sw-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// 배너 스와이퍼 기능
+const swBanner = new Swiper(".bannerSwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-    },
-  });
+});
 };
-// 배너 스와이퍼 기능
+
