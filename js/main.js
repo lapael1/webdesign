2e71ddfbe20bd6a9731d811e072b02558ac66248
lapael1 <lapael6@gmail.com>
2024-07-17 18:10:52 +0900

    마우스 오버 시 확대, 음영 조정 클릭 시 페이지로 이동

--- js/main.js
+++ js/main.js
@@ -62,4 +62,45 @@ const swBanner = new Swiper(".bannerSwiper", {
     },
 });
 };
+// top버튼 스크롤 기능=======================
+const topBtn = document.getElementById("top-btn");
+topBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log(window.scrollY);
+  if (window.scrollY == 0) {
+    window.scrollTo({
+      top: 2777,
+      behavior: "smooth",
+    });
+  } else {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+  const topBtn = document.querySelector(".top-btn");
+  topBtn.addEventListener("click", function () {
+    //  scrollToSection()함수는 특정 섹션으로 부드럽게 스크롤하는 기능
+    scrollToSection("#header");
+  });
+  //   scrollToSection()함수 작성
+  function scrollToSection(sectionId) {
+    // console.log(sectionId);
+    // sectionId로 지정된 요소를 찾아서 section변수에 저장
+    const section = document.querySelector(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+});
+//화살표 이미지 회전
+const topBtnImg = document.getElementById("top-btn-img");
+window.addEventListener("scroll", function (scTop) {
+  scTop = window.document.documentElement.scrollTop;
+  if (scTop > 0) {
+    topBtnImg.classList.add("up");
+  } else {
+    topBtnImg.classList.remove("up");
+  }
+});
 
